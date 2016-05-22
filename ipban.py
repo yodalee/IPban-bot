71dd0f9b4287a2226ff4a11901ba9977b21abc1a
yodalee <lc85301@gmail.com>
2016-05-22 16:31:26 +0800

    add FB webhook handler
    The verification code is now hardcoded in handler, which
    is not a good design. Use configuration reader or other
    method instead in the future.

--- ipban.py
+++ ipban.py
@@ -6,6 +6,19 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers["Content-Type"] = "text/plain"
         self.response.write("Hello, World!")
 
+
+class FBwebhook(webapp2.RequestHandler):
+    def get(self):
+        verification_code = "Verification code"
+        verify_token = self.request.get('hub.verify_token')
+        verify_challenge = self.request.get('hub.challenge')
+        if verification_code == verify_token:
+            self.response.write(verify_challenge)
+        else:
+            self.response.write("Error, wrong validation token")
+
+
 app = webapp2.WSGIApplication([
+    ('/webhook', FBwebhook),
     ('/', MainPage),
 ], debug=True)
