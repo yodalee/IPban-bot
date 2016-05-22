8dd83ed4d1fc8f03b69c1b0974f7ef6e31629096
yodalee <lc85301@gmail.com>
2016-05-22 17:54:56 +0800

    add response function

--- ipban.py
+++ ipban.py
@@ -1,4 +1,9 @@
 import webapp2
+import json
+from google.appengine.api import urlfetch
+
+
+FBtoken = "Fan book Token Here"
 
 
 class MainPage(webapp2.RequestHandler):
@@ -8,6 +13,22 @@ class MainPage(webapp2.RequestHandler):
 
 
 class FBwebhook(webapp2.RequestHandler):
+    def send_fb_message(self, to, message):
+        post_url = "https://graph.facebook.com/v2.6/me/messages?access_token={token}".format(
+                token=FBtoken)
+        response_message = json.dumps(
+            {"recipient": {"id": to},
+             "message": {"text": message}})
+        print(post_url)
+        print(response_message)
+        result = urlfetch.fetch(
+                url=post_url,
+                headers={"Content-Type": "application/json"},
+                payload=response_message,
+                method=urlfetch.POST)
+
+        print("[{}] reply to {}: {}".format(result, to, message))
+
     def get(self):
         verification_code = "Verification code"
         verify_token = self.request.get('hub.verify_token')
@@ -16,6 +37,19 @@ class FBwebhook(webapp2.RequestHandler):
             self.response.write(verify_challenge)
         else:
             self.response.write("Error, wrong validation token")
+
+    def post(self):
+        print(self.request.body)
+        message_entry = json.loads(self.request.body)['entry']
+
+        for entry in message_entry:
+            messagings = entry['messaging']
+            for message in messagings:
+                sender = message['sender']['id']
+                if message.get('message'):
+                    text = message['message']['text']
+                    print("{} says {}".format(sender, text))
+                self.send_fb_message(sender, "Hi")
 
 
 app = webapp2.WSGIApplication([
